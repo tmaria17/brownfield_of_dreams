277561b296e0065f3a0affcbeb09d1e508624cc9
Meghan Stang <mstang15@gmail.com>
2018-12-05 15:17:08 -0700

    adds vcr for github api

--- spec/features/user/user_can_see_repo_list_spec.rb
+++ spec/features/user/user_can_see_repo_list_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 describe 'A registered user' do
+
   it 'can display repo index if user has a token' do
     user = create(:user, token: ENV['github_token'])
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    visit '/dashboard'
 
-    expect(page).to have_content("Github")
-    expect(page).to have_css(".repo", count: 5)
+    VCR.use_cassette("github_token") do
+      visit '/dashboard'
 
-    within(first(".repo")) do
-      expect(page).to have_css(".name")
+      expect(page).to have_content("Github")
+      expect(page).to have_css(".repo", count: 5)
+
+      within(first(".repo")) do
+        expect(page).to have_css(".name")
+      end
     end
   end
 
@@ -28,11 +32,11 @@ describe 'A registered user' do
     user_2 = create(:user, token: ENV['github_token_2'])
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+    VCR.use_cassette("github_token") do
+      visit '/dashboard'
 
-    visit '/dashboard'
-    save_and_open_page
-    expect(page).to have_content("battleshift")
-    expect(page).to_not have_content("bradley_cooper")
-
+      expect(page).to have_content("battleshift")
+      expect(page).to_not have_content("bradley_cooper")
+    end
   end
 end
