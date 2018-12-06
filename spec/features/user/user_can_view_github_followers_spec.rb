2ba9aef069c0aa68dd2d1da553bc098a77b80dc3
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-05 17:30:26 -0700

    Finishes testing for followers.

--- spec/features/user/user_can_view_github_followers_spec.rb
+++ spec/features/user/user_can_view_github_followers_spec.rb
@@ -6,9 +6,12 @@ describe 'A registered user' do
     user = create(:user, token: ENV['github_token'])
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     visit '/dashboard'
-
+    save_and_open_page
     expect(page).to have_content("Github")
     expect(page).to have_content("Followers")
     expect(page).to have_css(".follower")
+    within(first(".follower")) do
+      expect(page).to have_css(".name")
+    end
   end
 end
