d51ad068e0c5606b9b52cf3136bd1e6eb58f210b
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-05 17:06:47 -0700

    Adds a spec for github followers.

---
+++ spec/features/user/user_can_view_github_followers_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'A registered user' do
+
+  it 'displays github followers' do
+    user = create(:user, token: ENV['github_token'])
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit '/dashboard'
+
+    expect(page).to have_content("Github")
+    expect(page).to have_content("Followers")
+    expect(page).to have_css(".follower")
+  end
+end
