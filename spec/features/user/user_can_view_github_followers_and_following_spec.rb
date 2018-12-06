0dd813d42d7e50f68b116b4778cfccedf525c6db
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-05 18:00:26 -0700

    Adds cassettes.

--- spec/features/user/user_can_view_github_followers_and_following_spec.rb
+++ spec/features/user/user_can_view_github_followers_and_following_spec.rb
@@ -6,13 +6,15 @@ describe 'A registered user' do
     user = create(:user, token: ENV['github_token'])
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit '/dashboard'
+    VCR.use_cassette("github_followers") do
+      visit '/dashboard'
 
-    expect(page).to have_content("Github")
-    expect(page).to have_content("Followers")
-    expect(page).to have_css(".follower")
-    within(first(".follower")) do
-      expect(page).to have_css(".name")
+      expect(page).to have_content("Github")
+      expect(page).to have_content("Followers")
+      expect(page).to have_css(".follower")
+      within(first(".follower")) do
+        expect(page).to have_css(".name")
+      end
     end
   end
 
@@ -20,13 +22,16 @@ describe 'A registered user' do
     user = create(:user, token: ENV['github_token'])
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit '/dashboard'
-    save_and_open_page
-    expect(page).to have_content("Github")
-    expect(page).to have_content("Following")
-    expect(page).to have_css(".followed_user")
-    within(first(".followed_user")) do
-      expect(page).to have_css(".name")
+    VCR.use_cassette("github_following") do
+
+      visit '/dashboard'
+      save_and_open_page
+      expect(page).to have_content("Github")
+      expect(page).to have_content("Following")
+      expect(page).to have_css(".followed_user")
+      within(first(".followed_user")) do
+        expect(page).to have_css(".name")
+      end
     end
   end
 
