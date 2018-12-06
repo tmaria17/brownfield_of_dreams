899b73e08dbb7f9ba038cdc5363187abe06e48e4
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-05 17:38:18 -0700

    Creates following spec.

--- spec/features/user/user_can_view_github_followers_and_following_spec.rb
+++ spec/features/user/user_can_view_github_followers_and_following_spec.rb
@@ -5,13 +5,31 @@ describe 'A registered user' do
   it 'displays github followers' do
     user = create(:user, token: ENV['github_token'])
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
     visit '/dashboard'
-    save_and_open_page
+
     expect(page).to have_content("Github")
     expect(page).to have_content("Followers")
     expect(page).to have_css(".follower")
     within(first(".follower")) do
       expect(page).to have_css(".name")
     end
+  end
+
+  it 'displays github followed users' do
+    user = create(:user, token: ENV['github_token'])
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/dashboard'
+
+    expect(page).to have_content("Github")
+    expect(page).to have_content("Following")
+    expect(page).to have_css(".followed_user")
+    within(first(".followed_user")) do
+      expect(page).to have_css(".name")
+    end
+  end
+
+
   end
 end
