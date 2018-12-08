2685af89f87dc80eaab317e648db9d5b9567fcf5
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-08 11:12:40 -0700

    Adds user from oauth method to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
   validates_presence_of :first_name
   enum role: [:default, :admin]
   has_secure_password
+
+
+  def self.from_omniauth(auth_info)
+    where(id: auth_info[:id]).first_or_create do |new_user|
+      user.email = auth.info.email
+      user.uid = auth.uid
+      user.provider = auth.provider
+      user.avatar_url = auth.info.image
+      user.username = auth.info.name
+      user.oauth_token = auth.credentials.token
+  end
+end
 end
