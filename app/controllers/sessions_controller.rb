059a753f8e1214799bffc8e91a7cbbe8c46c5fa2
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-08 11:09:39 -0700

    Sessions controller for Oauth wip.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
+
+    if params[:provider].present?
+      user = User.from_omniauth(request.env['omniauth.auth'])
       session[:user_id] = user.id
       redirect_to dashboard_path
-    else
-      flash[:error] = "Looks like your email or password is invalid"
-      render :new
     end
+    # else
+      # binding.pry
+      # user = User.find_by(email: params[:session][:email])
+      #  user && user.authenticate(params[:session][:password])
+      # session[:user_id] = user.id
+      # redirect_to dashboard_path
+    # end
+    # else
+    #   flash[:error] = "Looks like your email or password is invalid"
+    #   render :new
+    # end
   end
 
   def destroy
