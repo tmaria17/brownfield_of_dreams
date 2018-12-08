54a8ee099a96c4c20a4c8af863ffe3a85702b406
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-08 11:13:39 -0700

    Adds github env to this file.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 end
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], :provider_ignores_state => true
+  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'],:provider_ignores_state => true
 
 end
