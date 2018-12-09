1831f067a56bbcb9d6e411834a6d4390e5fd8468
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-09 11:22:22 -0700

    oauth spec.

---
+++ spec/features/oauth_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+  context 'as a signed in user' do
+    it 'connects to github' do
+      def stub_omniauth
+        OmniAuth.config.test_mode = true
+        OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+        'credentials' =>
+        {'token' =>  ENV['github_token']}
+        })
+      end
+
+      stub_omniauth
+      user = create(:user)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit '/dashboard'
+      click_link "Connect to Github"
+
+      # expect(user.token).to eq('00007')
+      expect(page).to have_content("Following")
+      expect(page).to have_content("Followers")
+      expect(page).to_not have_content("Connect to Github")
+    end
+  end
