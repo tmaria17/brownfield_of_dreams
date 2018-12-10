c7adf6c7f6d7dfa99c44549b46912c42ace141ad
Meghan Stang <mstang15@gmail.com>
2018-12-10 15:51:40 -0700

    adds test to add friends

--- spec/features/user/user_can_add_followers_as_friends_spec.rb
+++ spec/features/user/user_can_add_followers_as_friends_spec.rb
@@ -16,7 +16,7 @@ describe 'as a user' do
   end
 
   it 'users can use add friend link to actually add friends' do
-    user_1 = create(:user, token: ENV['github_token'])
+    user_1 = create(:user, token: ENV['github_token'], github_id: 12345678)
     user_2 = create(:user, token: ENV['github_token_2'], github_id: 37811063)
     user_3 = create(:user)
 
@@ -25,10 +25,11 @@ describe 'as a user' do
     visit dashboard_path
 
     click_on "Add Friend"
-
+    save_and_open_page
     within('.friends') do
-      expect(page).to have_content(user_2.name)
-      expect(page).to_not have_content(user_3.name)
+      expect(page).to have_content(user_2.first_name)
+      expect(page).to have_content(user_2.last_name)
+      expect(page).to_not have_content(user_3.first_name)
     end
   end
 end
