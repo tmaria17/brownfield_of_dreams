b1a3a9b5039bea48b940d3f4951ab4b9feeb9a36
Meghan Stang <mstang15@gmail.com>
2018-12-09 19:10:53 -0700

    adds new test for github friends

--- spec/features/user/user_can_add_followers_as_friends_spec.rb
+++ spec/features/user/user_can_add_followers_as_friends_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 describe 'as a user' do
   it 'users can add friends that also have accounts' do
-    
+    user_1 = create(:user, token: ENV['github_token'])
+    user_2 = create(:user, token: ENV['github_token_2'])
+    user_3 = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+    visit dashboard_path
+
+    expect(page).to have_link("Add Friend")
+
   end
 end
