0db813844cbef6c1f168c8579fb3ec171168ac2c
Meghan Stang <mstang15@gmail.com>
2018-12-10 16:05:52 -0700

    adds flash message

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,6 +3,8 @@ class FriendshipsController <ApplicationController
   def create
     friendship = current_user.friendships.create(friend_id: params[:friend_id])
     friendship.save
+    friend = User.find_by(github_id: params[:friend_id])
+    flash[:success] = "You have added #{friend.first_name} #{friend.last_name} as a friend."
     redirect_to dashboard_path
   end
 
