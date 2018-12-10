394a506b145df38770e920114c519e83fab657e9
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-10 15:41:18 -0700

    Frienship controller create

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,10 @@
-class FriendshipsController <ApplicationController
+class FriendshipsController < ApplicationController
 
   def create
-    binding.pry
+    # friend = User.find_by(github_id: params[:friend_id])
+    friendship = current_user.friendships.create(friend_id: params[:friend_id])
+    friendship.save
+    redirect_to dashboard_path
   end
 
 end
