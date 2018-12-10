bdd77494a3ce0ecb3ea4c432e4a9320941b638ac
Meghan Stang <mstang15@gmail.com>
2018-12-10 15:47:52 -0700

    creates new friendship and redirects to dashboard

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,9 @@
 class FriendshipsController <ApplicationController
 
   def create
-    binding.pry
+    friendship = current_user.friendships.create(friend_id: params[:friend_id])
+    friendship.save
+    redirect_to dashboard_path
   end
 
 end
