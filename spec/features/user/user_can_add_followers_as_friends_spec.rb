729afb0e5b2079b06fa036bdc1db30b82124c5e2
Meghan Stang <mstang15@gmail.com>
2018-12-10 16:06:36 -0700

    tests for flash message

--- spec/features/user/user_can_add_followers_as_friends_spec.rb
+++ spec/features/user/user_can_add_followers_as_friends_spec.rb
@@ -12,7 +12,9 @@ describe 'as a user' do
 
     visit dashboard_path
 
-    expect(page).to have_link("Add Friend")
+    within('.followers') do
+      expect(page).to have_link("Add Friend")
+    end
   end
 
   it 'users can use add friend link to actually add friends' do
@@ -23,13 +25,17 @@ describe 'as a user' do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
     visit dashboard_path
+    within('.followers') do
+      click_on "Add Friend"
+    end
 
-    click_on "Add Friend"
     save_and_open_page
+
     within('.friends') do
       expect(page).to have_content(user_2.first_name)
       expect(page).to have_content(user_2.last_name)
       expect(page).to_not have_content(user_3.first_name)
     end
+    expect(page).to have_content("You have added #{user_2.first_name} #{user_2.last_name} as a friend.")
   end
 end
