4302a8124148707d8b2eeb90f1f44c86df9d5978
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-10 17:09:18 -0700

    Adds logic for friendship path being invalid.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,11 +1,16 @@
 class FriendshipsController <ApplicationController
 
   def create
-    friendship = current_user.friendships.create(friend_id: params[:friend_id])
-    friendship.save
     friend = User.find_by(github_id: params[:friend_id])
-    flash[:success] = "You have added #{friend.first_name} #{friend.last_name} as a friend."
-    redirect_to dashboard_path
+      if friend == nil
+        flash[:error] = "This friendship could not be created. "
+        redirect_to dashboard_path
+      else
+        friendship = current_user.friendships.create(friend_id: params[:friend_id])
+        friendship.save
+        flash[:success] = "You have added #{friend.first_name} #{friend.last_name} as a friend."
+        redirect_to dashboard_path
+      end
   end
 
 end
