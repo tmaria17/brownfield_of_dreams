1b1f26c890e64b9f7fdd1daa767fff308a0e9617
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-11 13:44:21 -0700

    Tried validation wip.

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,10 +3,12 @@ class Video < ApplicationRecord
   has_many :users, through: :user_videos
   belongs_to :tutorial
   validates_presence_of :position
+  # before_validation :position_assignment, on: :create
+
 
   def self.position_assignment
     Video.where(position: nil).map do |video|
-      video.update_attribute(:position, Tutorial.find(video.tutorial_id).videos.maximum(:position)+1)
+      video.update_attributes(position: Tutorial.find(video.tutorial_id).videos.maximum(:position)+1)
       # binding.pry
     # Video.where(position: nil).map do |video|
     #   video.update(position: (Tutorial.find(video.tutorial_id)).videos.maximum(:position)+1)
