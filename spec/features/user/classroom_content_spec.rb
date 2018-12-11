9845e4c459465c3a318ee307dc830e180ae5f14b
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-10 19:25:43 -0700

    Adds test to make sure not logged in users can't see classroom content.

--- spec/features/user/classroom_content_spec.rb
+++ spec/features/user/classroom_content_spec.rb
@@ -1,15 +1,30 @@
 require 'rails_helper'
 
-describe 'as a logged in user' do
-  it 'displays content marked classroom content' do
+describe 'as a user' do
+  it 'displays content marked classroom content to logged in users' do
     user = create(:user, token: ENV['github_token_2'])
     tutorial = create(:tutorial, classroom: true)
     video1 = create(:video, tutorial_id: tutorial.id)
     video2 = create(:video, tutorial_id: tutorial.id)
+
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit tutorial_path(tutorial)
+    visit tutorials_path
 
     expect(page).to have_content(tutorial.title)
+  end
+
+  it 'does not display classroom content to users not logged in'do
+    user = create(:user)
+    tutorial = create(:tutorial, classroom: true)
+    video1 = create(:video, tutorial_id: tutorial.id)
+    video2 = create(:video, tutorial_id: tutorial.id)
+    tutorial_2 = create(:tutorial, classroom: false)
+
+    visit tutorials_path
+    
+    expect(page).to_not have_content(tutorial.title)
+    expect(page).to have_content(tutorial_2.title)
+
   end
 end
