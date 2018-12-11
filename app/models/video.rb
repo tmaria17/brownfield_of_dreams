fa47b9370223110c0580cd07f99b841d7226edd1
Meghan Stang <mstang15@gmail.com>
2018-12-11 10:01:24 -0700

    adds activerecord to get user videos

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,11 @@ class Video < ApplicationRecord
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial
+
+  def self.display_videos(user)
+    joins(:user_videos)
+    .order(:tutorial_id, :position)
+    .where('user_id = ?', user.id)
+    .limit(5)
+  end
 end
