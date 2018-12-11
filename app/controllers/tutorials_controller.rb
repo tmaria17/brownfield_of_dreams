8e3965312000384830a1b3382ac5000c69d2b6f8
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-10 21:53:39 -0700

    Logic for show page with classroom content.

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,13 +1,18 @@
 class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
-    @facade = TutorialFacade.new(tutorial, params[:video_id])
+    if current_user == nil && tutorial[:classroom] == true
+      redirect_to tutorials_path
+      flash[:error] = "Please log in to view this tutorial."
+    else
+      @facade = TutorialFacade.new(tutorial, params[:video_id])
+    end
   end
 
   def index
     if current_user == nil
       @tutorials = TutorialFacade.new(Tutorial.all.where(classroom: false))
-    elsif current_user != nil
+    elsif current_user != nils
       @tutorials = TutorialFacade.new(Tutorial.all)
     end
   end
