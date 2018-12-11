0956369893008ece3a42cd42bd413a14e36a3f68
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-10 19:24:50 -0700

    Adds index to tutorial controller and adds logic if tutorial has classroom content that is false.

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -3,4 +3,13 @@ class TutorialsController < ApplicationController
     tutorial = Tutorial.find(params[:id])
     @facade = TutorialFacade.new(tutorial, params[:video_id])
   end
+
+  def index
+    if current_user == nil
+      @tutorials = TutorialFacade.new(Tutorial.all.where(classroom: false))
+    elsif current_user != nil
+      @tutorials = TutorialFacade.new(Tutorial.all)
+    end
+  end
+
 end
