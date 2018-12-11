36b8832359c37efb8754b5a6255f3b7e17563c04
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-11 12:09:27 -0700

    position assignment method wip.

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,4 +3,16 @@ class Video < ApplicationRecord
   has_many :users, through: :user_videos
   belongs_to :tutorial
   validates_presence_of :position
+
+  def self.position_assignment
+    Video.where(position: nil).map do |video|
+      video.update(position: (Tutorial.find(video.tutorial_id)).videos.maximum(:position)+1)
+      # binding.pry
+    # Video.where(position: nil).map do |video|
+    #   video.update(position: (Tutorial.find(video.tutorial_id)).videos.maximum(:position)+1)
+    # end
+    end
+    # binding.pry
+
+  end
 end
