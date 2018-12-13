7a78d2fb4eba6350c0c20e857fc73beaa6555383
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-13 16:02:33 -0700

    invite emails done, all tests passing.

--- spec/features/user/user_can_send_invitations_spec.rb
+++ spec/features/user/user_can_send_invitations_spec.rb
@@ -9,13 +9,14 @@ describe 'as a user' do
     click_on "Send an Invite"
 
     expect(current_path).to eq("/invite")
+
     fill_in :github_handle, with: "jplao"
     click_on "Send Invite"
+
     expect(current_path).to eq("/dashboard")
     expect(page).to have_content("Successfully sent invite!")
-  #   if the user has an email address associated with their github account
-  #   "The Github user you selected doesn't have an email address associated with their account."
   end
+
   it 'displays an error if the user does not have a public email' do
     user = create(:user, token: ENV['github_token'], github_id: 123456)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -24,8 +25,12 @@ describe 'as a user' do
     click_on "Send an Invite"
 
     expect(current_path).to eq("/invite")
+
     fill_in :github_handle, with: "tmaria17"
     click_on "Send Invite"
+
     expect(page).to have_content("The Github user you selected doesn't have an email address associated with their account.")
+    expect(current_path).to eq("/dashboard")
+
   end
 end
