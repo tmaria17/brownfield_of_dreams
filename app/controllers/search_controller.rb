71b2dd94d483d94d041d1588db30cd1687e45e51
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-13 15:43:58 -0700

    Adds search controller for handles.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,14 @@
 class SearchController < ApplicationController
   def index
+    if params[:github_handle]
+      @invitee = Search.new(params[:github_handle],current_user.token).invitee_info
+      GithubMailer.invite(@invitee, current_user).deliver_now
+        if @invitee[:email] != nil
+          flash[:success] = "Successfully sent invite!"
+        else
+          flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
+        end
+      redirect_to dashboard_path
+   end
   end
 end
