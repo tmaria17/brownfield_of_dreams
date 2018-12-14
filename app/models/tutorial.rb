1ce0ced9f928e56ffd05cc78b404b6c8736946ae
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-14 00:58:22 -0700

    Tutorial method to get public content.

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,4 +2,9 @@ class Tutorial < ApplicationRecord
   has_many :videos, ->  { order(position: :ASC) }
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
+
+  def self.public_content
+    Tutorial.where(classroom: false)
+  end
+  
 end
