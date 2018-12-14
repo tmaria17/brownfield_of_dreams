e975426b67a8754d8968cb31af6eb4f6dcbc1fe2
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-14 01:16:13 -0700

    Relationship testing

--- spec/models/user_video_spec.rb
+++ spec/models/user_video_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe UserVideo, type: :model do
+  describe 'realationships' do
+    it {should belong_to(:video)}
+    it {should belong_to(:user)}
+
+  end
 end
