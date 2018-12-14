2a5b9bf53d90eab38bb5b5525ca927754c5652c8
Meghan Stang <mstang15@gmail.com>
2018-12-13 17:57:44 -0700

    fixes spacing and indentation

--- app/mailers/github_mailer.rb
+++ app/mailers/github_mailer.rb
@@ -3,7 +3,6 @@ class GithubMailer < ApplicationMailer
   def invite(invitee, current_user)
     @invitee = invitee
     @user = current_user
-    # binding.pry
     mail(to: @invitee[:email], subject: "Invitation to Turing Tutorials")
   end
 end
